9de874a9c1dc90cafc82b04a4f709a4f996877ae
Preston <prestonosoro56@gmail.com>
2023-08-17 00:10:05 +0300

    feat: create reusable range function

--- libs/utils/src/lib/utils.ts
+++ libs/utils/src/lib/utils.ts
@@ -1,3 +1,11 @@
+/** 
 export function utils(): string {
   return 'utils';
 }
+*/
+
+// create a reusable function
+
+export const range = (start: number, end: number): number []=> {
+  return [...Array(end-start).keys()].map((el) => el + start);
+};
