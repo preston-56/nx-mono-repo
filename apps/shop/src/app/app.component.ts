b878bafaabff44e65f20b378f39b913f2f0bd67a
Preston <prestonosoro56@gmail.com>
2023-08-17 00:09:07 +0300

    feat: import range

--- apps/shop/src/app/app.component.ts
+++ apps/shop/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {range} from '@org/utils'
 
 @Component({
   standalone: true,
@@ -9,6 +10,10 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shop';
+
+  ngOnInit(): void {
+    console.log(range(40,45));
+  }
 }
